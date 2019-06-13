45031b0467091995121f482e3ed9895e4d9d79cb
zhangpengfei <zhangpengfei@sensetime.com>
2019-06-13 15:03:22 +0800

    init project

---
+++ app/src/main/java/com/afei/camera2getpreview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.afei.camera2getpreview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
